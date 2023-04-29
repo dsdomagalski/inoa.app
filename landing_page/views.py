0e973d28578c7ac3cde38a00930ebab531da4c0c
dsdomagalski <90064086+dsdomagalski@users.noreply.github.com>
2023-04-29 16:27:29 -0300

    Inclusão da home

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def landing_page_index(request):
-    return HttpResponse("Hey! It works!")
+def home(request):
+    return render(request, 'home.html')
